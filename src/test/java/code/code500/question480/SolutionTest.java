c7356ed049a47f458188bb404cec093c92658d23
AziCat <1003980136@qq.com>
2021-02-03 16:39:29 +0800

    2021.02.03提交

--- src/test/java/code/code500/question480/SolutionTest.java
+++ src/test/java/code/code500/question480/SolutionTest.java
@@ -13,13 +13,4 @@ public class SolutionTest extends TestCase {
         double [] result = solution.medianSlidingWindow(new int[]{-2147483648,-2147483648,2147483647,-2147483648},2);
     }
 
-    public void test1(){
-        PriorityQueue<Integer> maxTop = new PriorityQueue<>((o1, o2) -> Integer.compare(o2,o1));
-        for (int i = 0; i < 10; i++) {
-            maxTop.offer(i);
-        }
-        while (!maxTop.isEmpty()) {
-            System.out.println(maxTop.poll());
-        }
-    }
 }
